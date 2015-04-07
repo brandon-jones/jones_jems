62fe25340427cd8e8abbfe881d7aaa637dc9b1fd
brandon jones <brandon.jones9@gmail.com>
2015-04-06 21:36:48 -0700

    fixed some logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticated_admin?, except: [:new, :show, :edit, :create, :update]
+  before_action :non_auth_only, only: :new
+
+  def non_auth_only
+    redirect_to root_url if current_user
+  end
   # GET /users
   # GET /users.json
   def index
