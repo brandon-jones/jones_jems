85632d6abf567740d7a4e99b090d6f85e6997974
brandon jones <brandon.jones9@gmail.com>
2015-04-08 15:48:57 -0700

    use js for home image now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,32 @@
 //= require papercrop
 //= require dropzone
 //= require_tree .
+
+
+
+
+$(document).ready(function() {
+	jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+	}
+  return centerImage();
+});
+
+window.onresize = function(event) { 
+	centerImage();
+};
+
+centerImage = function(e) {
+	var image = $('#js-center-image');
+	if (image) {
+		image.center();
+	}
+	
+	
+};
+
