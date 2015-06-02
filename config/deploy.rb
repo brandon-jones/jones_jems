16c4e20e5080001331830d49f54865b91e060a48
brandon jones <brandon.jones9@gmail.com>
2015-06-02 12:47:21 -0700

    maybe

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,14 +56,19 @@ namespace :deploy do
 
 end
 
-desc 'copy ckeditor nondigest assets'
-task :copy_nondigest_assets do
-  on roles :app do
-    within fetch(:release_path) do
-      with rails_env: fetch(:rails_env) do
-        execute :rake, "ckeditor:create_nondigest_assets"
-      end
+namespace :ckeditor do
+  desc 'Create nondigest versions of some ckeditor assets (e.g. moono skin png)'
+  task :create_nondigest_assets do
+    fingerprint = /\-[0-9a-f]{32}\./
+    for file in Dir['public/assets/ckeditor/contents-*.css', 'public/assets/ckeditor/skins/moono/*.png']
+      next unless file =~ fingerprint
+      nondigest = file.sub fingerprint, '.' # contents-0d8ffa186a00f5063461bc0ba0d96087.css => contents.css
+      FileUtils.cp file, nondigest, verbose: true
     end
   end
 end
-after 'deploy:assets:precompile', 'copy_nondigest_assets'
+
+# auto run ckeditor:create_nondigest_assets after assets:precompile
+Rake::Task['assets:precompile'].enhance do
+  Rake::Task['ckeditor:create_nondigest_assets'].invoke
+end
