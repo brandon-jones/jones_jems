57f1123cafb69cea71f789bebb434a1ab0f3075e
brandon jones <brandon.jones9@gmail.com>
2015-06-02 12:44:18 -0700

    maybe

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,3 +55,15 @@ namespace :deploy do
   end
 
 end
+
+desc 'copy ckeditor nondigest assets'
+task :copy_nondigest_assets do
+  on roles :app do
+    within fetch(:release_path) do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "ckeditor:create_nondigest_assets"
+      end
+    end
+  end
+end
+after 'deploy:assets:precompile', 'copy_nondigest_assets'
