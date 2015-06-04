836989c0e953c802bbc48b708b3fe0496bdc3917
brandon jones <brandon.jones9@gmail.com>
2015-06-03 20:43:13 -0700

    no idea

--- app/models/about_me.rb
+++ app/models/about_me.rb
@@ -16,11 +16,4 @@ class AboutMe
     @description = data[:description]
     return self
   end
-
-  private
-
-  def file_path
-    return "config/about_me.txt" if Rails.env == 'development'
-    return "/home/deployer/apps/jones_jems/shared/config/about_me.txt"
-  end
 end
