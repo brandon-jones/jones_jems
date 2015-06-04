dc9b5c2e6c6df396760917a2176bcec3ebd1a115
brandon jones <brandon.jones9@gmail.com>
2015-06-03 20:41:41 -0700

    umm

--- app/models/about_me.rb
+++ app/models/about_me.rb
@@ -3,14 +3,14 @@ class AboutMe
   attr_accessor :description
 
   def initialize
-    File.open(file_path, "r") do |f|
+    File.open("config/about_me.txt", "r") do |f|
       @description = f.read
     end
     return self
   end
 
   def update(data)
-    File.open(file_path, "w+") do |f|
+    File.open("config/about_me.txt", "w+") do |f|
       f.write(data[:description])
     end
     @description = data[:description]
